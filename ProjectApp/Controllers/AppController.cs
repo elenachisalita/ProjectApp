eb4432d04fd2193ea032efa87aaf68b7a858cf33
elenachisalita <elena.chisalita95@yahoo.com>
2020-06-23 19:59:28 +0300

    bootstrap elements added

--- ProjectApp/Controllers/AppController.cs
+++ ProjectApp/Controllers/AppController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjectApp.Services;
+using ProjectApp.ViewModels;
 
 namespace ProjectApp.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
           
@@ -18,12 +25,24 @@ namespace ProjectApp.Controllers
 
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
-            throw new InvalidOperationException("Bad things happen");
-
+       
+          
             return View();
         }
-
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+       
+            if(ModelState.IsValid)
+            {
+                //send the email
+                _mailService.SendMessage("sawn@yahoo.com", model.Subject, $"From: {model.Name} - {model.Email},Message: {model.Message})");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+         
+            return View();
+        }
         public IActionResult About()
         {
             ViewBag.Title = "About Us";
