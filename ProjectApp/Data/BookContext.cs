2c4a3fec92cb32225546a71090677f95ee563ee2
elenachisalita <elena.chisalita95@yahoo.com>
2020-06-23 21:07:44 +0300

    database created

---
+++ ProjectApp/Data/BookContext.cs
@@ -0,0 +1,20 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectApp.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options): base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order>Orders { get; set; }
+
+    }
+}
--- ProjectApp/Startup.cs
+++ ProjectApp/Startup.cs
@@ -5,18 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjectApp.Data;
 using ProjectApp.Services;
 
 namespace ProjectApp
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookContext>(cfg => 
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("BookConnectionString"));
+            });
             
             services.AddTransient<IMailService, NullMailService>();
             services.AddMvc();
